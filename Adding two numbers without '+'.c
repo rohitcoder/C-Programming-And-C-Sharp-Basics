455cdc0517077057a5eadaf7a4d2abf6c847094c
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-07 12:33:03 +0530

    Update Adding two numbers without '+'.c

--- Adding two numbers without '+'.c
+++ Adding two numbers without '+'.c
@@ -15,6 +15,45 @@ int add(int a, int b)
         return a;
       else
         sum = a^b; // add without carrying
-        carry = (a&b) << 1; // carry, but don’t add
+        carry = (a&b) << 1; // carry, but donâ€™t add
       return add(sum,carry); 
 }
+/*
+in c sharp
+
+class Program
+{ 
+  public int add(int a,int b)
+ {
+  int sum;
+  int carry;
+    if(b  == 0)
+    {
+    return a;
+    }
+    else
+    {
+    sum = a^b;
+    carry = (a&b) << 1;
+    return addd(sum,carry);
+    } 
+  }
+  public static void Main(string[] args)
+  {
+   int x;
+   int y;
+   int sum;
+   Console.WriteLine("Enter the values of A and B :");
+   int a = Convert.ToInt32(Console.ReadLine());
+   int b = Convert.ToInt32(Console.ReadLine());
+   sum = add(x,y);
+   Console.WriteLine(sum);
+
+
+  }
+}
+
+
+
+
+*/
